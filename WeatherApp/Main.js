548dcf92724f822bd84fc335121dd26dc6a2eb09
19639804@qq.com <19639804@qq.com>
2016-12-03 22:24:49 +0800

    Chaneg Ajax to Jsonp

--- WeatherApp/Main.js
+++ WeatherApp/Main.js
@@ -1,19 +1,33 @@
 /**
  * Created by apple on 16/12/3.
  */
-var getCityWeather = {
-    xhr : new XMLHttpRequest(),
-    json : '',
-    getWeather : function fn() {
-        var that = this;
-        that.xhr.open('get', 'http://api.jirengu.com/weather.php');
-        that.xhr.onreadystatechange = function(){
-            that.json = JSON.parse(that.xhr.responseText);
-            render(that.json);
-        };
-        that.xhr.send();
-    }
-};
+// var getCityWeather = {
+//     xhr : new XMLHttpRequest(),
+//     json : '',
+//     getWeather : function fn() {
+//         var that = this;
+//         that.xhr.open('get', 'http://api.jirengu.com/weather.php');
+//         that.xhr.onreadystatechange = function(){
+//             that.json = JSON.parse(that.xhr.responseText);
+//             render(that.json);
+//         };
+//         that.xhr.send();
+//     }
+// };
+
+//getCityWeather.getWeather();
+
+
+//      上面使用了 AJAX 跨域因为 GitPage 使用了 HTTPS
+//      所以 AJAX 失效了 所以改写成 JSONP 其中 AJAX 在上
+//      JSONP 的方法在下
+
+function addScriptTag(src){
+    var script = document.createElement('script');
+    script.setAttribute("type","text/javascript");
+    script.src = src;
+    document.body.appendChild(script);
+}
 function render(json) {
     var currentCity = json.results[0].currentCity;
     var city = document.querySelector('#currentCity');
@@ -25,4 +39,4 @@ function render(json) {
     var wea = json.results[0].weather_data[0].weather;
     weather.innerHTML = wea;
 };
-getCityWeather.getWeather();
+addScriptTag("http://api.jirengu.com/weather.php?callback=render");
